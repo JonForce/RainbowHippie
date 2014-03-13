b54a39c4014175dcf01683f1774204a801261e55
Toby Simone <pancakesimone@gmail.com>
2014-03-13 19:03:56 -0400

    Added new movement code - needs to be fine tuned

--- src/core/RainbowHippie.java
+++ src/core/RainbowHippie.java
@@ -10,71 +10,79 @@ import com.badlogic.gdx.math.Rectangle;
 import com.badlogic.gdx.math.Vector2;
 
 public class RainbowHippie implements Renderable, Tickable {
-	
+
 	public static RainbowHippie activeHippie;
-	
-	//Enumerator
+
+	// Enumerator
 	public static final int FLYING = 0;
 	public static final int HIDDEN = 1;
 	public static final int HOLDING = 2;
-	
-	//Other immutables
-	public final Vector2 hippieSize = new Vector2(AssetManager.fly.getWidth()/7, AssetManager.fly.getHeight());
+
+	// Other immutables
+	public final Vector2 hippieSize = new Vector2(AssetManager.fly.getWidth() / 7, AssetManager.fly.getHeight());
+	public Vector2 flyForce = new Vector2(1.0f, 1.0f);
 	public final int delayAfterDeath = 50;
-	
-	//Rendering
+
+	// Rendering
 	public Texture activeTexture;
 	public int srcX, srcY, srcWidth, srcHeight;
+	public float gravity = -0.5f;
 	public int state = HOLDING;
 	public boolean isDead = false;
 	private int frame = 0;
 	public StartSign sign = new StartSign();
-	public GuiImage logo = new GuiImage(AssetManager.logo, new Vector2(Game.center.x-(AssetManager.logo.getWidth()/2), Game.center.y));
+	public GuiImage logo = new GuiImage(AssetManager.logo, new Vector2(Game.center.x - (AssetManager.logo.getWidth() / 2), Game.center.y));
 	private RainbowRay rainbowRay;
-	
-	//Positional and movement
+
+	// Positional and movement
 	public Vector2 location;
 	public float damping = .15f;
 	public boolean lockedX = false;
 	public boolean lockedY = false;
-	
-	//Collision
+
+	// Collision
 	public Rectangle boundingBox;
-	
+
 	public RainbowHippie() {
 		activeHippie = this;
 		location = Game.startPosition;
 		activeTexture = AssetManager.fly;
 		Game.activeGame.toBeRendered.add(this);
 		Game.activeGame.toBeTicked.add(this);
-		boundingBox = new Rectangle(0,0,hippieSize.x-30, hippieSize.y-30);
+		boundingBox = new Rectangle(0, 0, hippieSize.x - 30, hippieSize.y - 30);
 	}
-	
+
 	@Override
 	public void render() {
 		if (state != HIDDEN)
-			Game.activeGame.batch.draw(activeTexture, location.x, location.y,
-										srcX, srcY, srcWidth, srcHeight);
+			Game.activeGame.batch.draw(activeTexture, location.x, location.y, srcX, srcY, srcWidth, srcHeight);
 	}
-	
+
 	@Override
 	public void tick() {
-		//Move bounding box to the center of our hippie
-		boundingBox.setCenter(new Vector2(location.x+hippieSize.x/2, location.y+hippieSize.y/2));
-		
-		//Update based on state
+		// Move bounding box to the center of our hippie
+		boundingBox.setCenter(new Vector2(location.x + hippieSize.x / 2, location.y + hippieSize.y / 2));
+
+		// Update based on state
 		if (state == FLYING) {
-			//Animate
+			// Animate
 			animate(7, AssetManager.fly);
 			sign.visible = false;
+
+			if(Gdx.input.isTouched()) flyForce.y += 10.0f;
 			
-			//Deltas are the desired position-actualPosition
-			float deltaY = Game.screenSize.y - Gdx.input.getY()-(location.y+90);
-			float deltaX = 150-(location.x+100);
-			//Amount to moves are the deltas*daming
-			float amountToMoveY = deltaY*damping;
-			float amountToMoveX = deltaX*(damping/4);
-			//Move by the amount to move
+			flyForce.y += gravity;
+
+			if(flyForce.y >= 0.6f) flyForce.y = 0.6f;
+			
+			
+			// Deltas are the desired position-actualPosition
+			float deltaY = flyForce.y * location.y;
+			float deltaX = 50 - location.x;
+			// Amount to moves are the deltas*daming
+			float amountToMoveY = deltaY * damping;
+			float amountToMoveX = deltaX * (damping / 4);
+			// Move by the amount to move
 			if (!lockedX)
 				location.x += amountToMoveX;
 			if (!lockedY)
@@ -83,59 +91,61 @@ public class RainbowHippie implements Renderable, Tickable {
 			animate(7, AssetManager.flyHold);
 			sign.useFrame(frame);
 			sign.visible = true;
-			
-			//Assert that hippie may not move, and that he is at the start position
+
+			// Assert that hippie may not move, and that he is at the start
+			// position
 			lockedX = true;
 			lockedY = true;
 			location = Game.startPosition;
 		} else if (state == HIDDEN) {
-			//Uses the frame as a timer to wait the delay before displaying the death screen
+			// Uses the frame as a timer to wait the delay before displaying the
+			// death screen
 			if (frame >= delayAfterDeath) {
-				
+
 			} else {
-				frame ++;
+				frame++;
 			}
 		}
-		
-		//Assert that our hippie does not leave the playing area
+
+		// Assert that our hippie does not leave the playing area
 		assertOnScreen();
 	}
-	
+
 	public void die() {
-		new FlyingBodyPart(new Vector2(location.x, location.y+50), FlyingBodyPart.HEAD).velocity = new Vector2(-5, 10);
-		new FlyingBodyPart(new Vector2(location.x, location.y+10), FlyingBodyPart.BODY).torque = 5f;
-		new FlyingBodyPart(new Vector2(location.x+20, location.y+10), FlyingBodyPart.ARM).velocity = new Vector2(-10, 5);
-		new FlyingBodyPart(new Vector2(location.x+5, location.y+10), FlyingBodyPart.ARM).velocity = new Vector2(-6, 3);
-		new FlyingBodyPart(new Vector2(location.x+5, location.y), FlyingBodyPart.LEG).velocity = new Vector2(-10, -5);
-		new FlyingBodyPart(new Vector2(location.x+15, location.y), FlyingBodyPart.LEG).velocity = new Vector2(-10, -5);
+		new FlyingBodyPart(new Vector2(location.x, location.y + 50), FlyingBodyPart.HEAD).velocity = new Vector2(-5, 10);
+		new FlyingBodyPart(new Vector2(location.x, location.y + 10), FlyingBodyPart.BODY).torque = 5f;
+		new FlyingBodyPart(new Vector2(location.x + 20, location.y + 10), FlyingBodyPart.ARM).velocity = new Vector2(-10, 5);
+		new FlyingBodyPart(new Vector2(location.x + 5, location.y + 10), FlyingBodyPart.ARM).velocity = new Vector2(-6, 3);
+		new FlyingBodyPart(new Vector2(location.x + 5, location.y), FlyingBodyPart.LEG).velocity = new Vector2(-10, -5);
+		new FlyingBodyPart(new Vector2(location.x + 15, location.y), FlyingBodyPart.LEG).velocity = new Vector2(-10, -5);
 		state = HIDDEN;
 		frame = 0;
 		isDead = true;
 	}
-	
+
 	/*
-	 * Sets the activeSpritesheet, srcX, width and height. 
+	 * Sets the activeSpritesheet, srcX, width and height.
 	 */
 	private void animate(int frames, Texture spriteSheet) {
-		if (frame >= frames-1)
+		if (frame >= frames - 1)
 			frame = 0;
 		else
-			frame ++;
-		
+			frame++;
+
 		activeTexture = spriteSheet;
-		srcX = frame*(spriteSheet.getWidth()/frames);
+		srcX = frame * (spriteSheet.getWidth() / frames);
 		srcY = 0;
-		srcWidth = (spriteSheet.getWidth()/frames);
+		srcWidth = (spriteSheet.getWidth() / frames);
 		srcHeight = spriteSheet.getHeight();
 	}
-	
+
 	/*
-	 * Creates an upper and lower bounds of where the hippie is,
-	 * by checking if he is above the top or below the bottom. Called every frame.
+	 * Creates an upper and lower bounds of where the hippie is, by checking if
+	 * he is above the top or below the bottom. Called every frame.
 	 */
 	public void assertOnScreen() {
-		if (location.y >= (int)Game.screenSize.y-200) {
-			location.y = (int)Game.screenSize.y-200;
+		if (location.y >= (int) Game.screenSize.y - 200) {
+			location.y = (int) Game.screenSize.y - 200;
 		} else if (location.y <= 30) {
 			location.y = 30;
 		}
